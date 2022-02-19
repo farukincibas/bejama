bd5955baf1e3fb85ac82c0a03fda29246f164286
omer faruk incibas <farukincibas@hotmail.com>
2022-02-20 00:47:54 +0300

    imp(product-list): created product list area

--- src/components/ProductListArea/index.styles.ts
+++ src/components/ProductListArea/index.styles.ts
@@ -7,10 +7,19 @@ interface SortImageProps {
 
 export const SortArrowImage = styled.img<SortImageProps>`
     ${({ source }) => css`
-    opacity: 0.5;
-    content: url( ${source});
+        opacity: 0.5;
+        content: url( ${source});
     `}
   `;
+
+  export const SettingsImage = styled.img<SortImageProps>`
+    ${({ source }) => css`
+        width: 100%;
+        object-fit: contain;
+        content: url( ${source});
+    `}
+  `;
+
 
 export const ProductListHeadDiv = styled.div`
     display: flex;
@@ -43,10 +52,46 @@ export const ProductListSortDiv = styled.div`
     position: relative;
     padding-right: 12px;
     cursor: pointer;
+    @media (max-width: 999px) {
+        display: none;
+    }
 `;
 
 export const LightTextDiv = styled.div`
     opacity: 0.7;
 `;
 
+export const SelectPrice = styled.select<SortImageProps>`
+    ${({ source, theme: { colors } }) => css`
+        padding: 3px 5px;
+        padding-right: 17px;
+        appearance: none;
+        -webkit-appearance: none;
+        border: none;
+        background: ${colors.WHITE};
+        background-image: url(${source});
+        background-repeat: no-repeat;
+        background-position: right;
+        background-size: 10px;
+        @media (max-width: 999px) {
+            display: none;
+        }
+    `}
+`;
+
+
+export const ProductListMobileSortDiv = styled.div`
+    ${({ theme: { colors } }) => css`
+        width: 30px;
+        height: 30px;
+        margin-left: 12px;
+        padding: 7px;
+        border: 1px solid ${colors.DARK};
+        border-radius: 7px;
+        display: none;
+        @media (max-width: 999px) {
+            display: block;
+        }
+    `}
+`;
 
--- src/components/ProductListArea/index.tsx
+++ src/components/ProductListArea/index.tsx
@@ -1,6 +1,7 @@
-import { HeavySpan, LightFiftySpan, LightTextDiv, ProductListFilterDiv, ProductListHeadDiv, ProductListSortDiv, ProductListTypeDiv, SortArrowImage } from './index.styles';
+import { HeavySpan, LightFiftySpan, LightTextDiv, ProductListFilterDiv, ProductListHeadDiv, ProductListMobileSortDiv, ProductListSortDiv, ProductListTypeDiv, SelectPrice, SettingsImage, SortArrowImage } from './index.styles';
 import Sort from '../../icons/sort.svg';
-
+import CarretDown from '../../icons/carret-down.svg';
+import SettingsIcon from '../../icons/setting-lines.svg';
 const ProductListArea = () => {
     return (
         <>
@@ -16,7 +17,20 @@ const ProductListArea = () => {
                         <SortArrowImage source={Sort}></SortArrowImage>
                         <LightTextDiv>Sort</LightTextDiv>
                     </ProductListSortDiv>
+
+                    {/* price */}
+                    <SelectPrice source={CarretDown}>
+                        <option value="">Price</option>
+                        <option value="Ascending">Ascending</option>
+                        <option value="Descending">Descending</option>
+                    </SelectPrice>
+
+                    {/* mobile tap filter */}
+                    <ProductListMobileSortDiv>
+                        <SettingsImage source={SettingsIcon}></SettingsImage>
+                    </ProductListMobileSortDiv>
                     
+
                 </ProductListFilterDiv>
             </ProductListHeadDiv>
         </>
